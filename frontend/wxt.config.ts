3b5ccc7ebcba02323077309b03dd719475021166
wulinyi <wulinyi@vendetech.com>
2026-02-27 23:00:45 +0800

    chore:  use the src directory, add shadcn UI
    - ./entrypoints -> ./src/entrypoints

--- frontend/wxt.config.ts
+++ frontend/wxt.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  srcDir: 'src',
   vite: () => ({
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   }),
 });
